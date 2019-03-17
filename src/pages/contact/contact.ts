1e1c2834cd962ed1acff86da5a6778aa40a3a997
siwat jomewattana <s1104300051612@gmail.com>
2019-03-17 11:32:19 +0700

    ฟังชั่น console.log ใน javascrip

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -10,7 +10,7 @@ export class ContactPage {
   items = ["Siwat","Premruthai","Aun","Yanisa","Chanyanuch"]; // ประกาศตัวแปร Array ชื่อ items
   
   itemSelected(item : string){ // สร้างฟังชั่นหรือเมธอทชื่อ itemSelected มีพารามิเตอร์ ชื่อ item ชนิด String
-    console.log("Selected Item: ",item);
+    console.log("Selected Item: ",item); //console.log จะเป็นฟังชั่นเทสการส่งค่าแล้วแสดงผล Selected Item: กับ สิ่งที่คลิ๊กว่าส่งมาแล้วรึยัง โดยการกด F12 ในเว็บเบาเซอร์
   }
 
   constructor(public navCtrl: NavController) {
